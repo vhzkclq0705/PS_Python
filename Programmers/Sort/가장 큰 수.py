7435028cd4a7f3b83406fc0d2a978c2256fa81e8
oh_jun <vhzkclq0705@naver.com>
2022-09-17 18:14:08 +0900

    가장 큰 수

---
+++ Programmers/Sort/가장 큰 수.py
@@ -0,0 +1,10 @@
+# Level 2
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x * 3, reverse = True)
+    
+    return str(int(''.join(numbers)))
+
+n = [6, 10, 2]
+print(solution(n))
