d705b6517593282ab1d52a3b83b1cc76be337930
oh_jun <vhzkclq0705@naver.com>
2023-01-04 21:30:18 +0900

    15649

--- BackJoon/BackTracking/15649.py
+++ BackJoon/BackTracking/15649.py
@@ -1,7 +1,25 @@
 # Silver 3 - N과 M (1)
 
+# Permutation
 from itertools import permutations
 
 n, m = map(int, input().split())
 for per in permutations([i for i in range(1, n + 1)], m):
     print(*per)
+
+# BackTracking
+n, m = map(int, input().split())
+s = []
+
+def bt():
+    if len(s) == m:
+        print(*s)
+        return
+
+    for i in range(1, n + 1):
+        if i not in s:
+            s.append(i)
+            bt()
+            s.pop()
+
+bt()
