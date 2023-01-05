c765a77f7b6891ee56b3e482184fd23c80b7bf14
oh_jun <vhzkclq0705@naver.com>
2023-01-05 18:41:04 +0900

    15650

---
+++ BackJoon/BackTracking/15650.py
@@ -0,0 +1,26 @@
+# Silver 3 - N과 M (2)
+
+# Combination
+# from itertools import combinations
+
+# n, m = map(int, input().split())
+# s = [i for i in range(1, n + 1)]
+
+# for com in combinations(s, m):
+#     print(*map(int, com))
+
+# BackTracking
+n, m = map(int, input().split())
+s = []
+
+def bt(v):
+    if len(s) == m:
+        print(*s)
+        return
+    
+    for i in range(v, n + 1):
+        s.append(i)
+        bt(i + 1)
+        s.pop()
+
+bt(1)
