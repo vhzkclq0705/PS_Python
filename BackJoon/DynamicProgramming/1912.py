dcb7c19424b0c34c4d21b8f4b91e97d863bb314e
oh_jun <vhzkclq0705@naver.com>
2023-01-09 01:52:36 +0900

    1912.py

---
+++ BackJoon/DynamicProgramming/1912.py
@@ -0,0 +1,11 @@
+# Silver 2 - 연속합
+
+n = int(input())
+s = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = s[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + s[i], s[i])
+
+print(max(dp))
