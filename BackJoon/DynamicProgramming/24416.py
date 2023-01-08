48ca288956f9c3f1e0e7378f9b8c2f14d0df4654
oh_jun <vhzkclq0705@naver.com>
2023-01-09 00:06:49 +0900

    24416

--- BackJoon/DynamicProgramming/24416.py
+++ BackJoon/DynamicProgramming/24416.py
@@ -1,20 +1,9 @@
 # Bronze 1 - 알고리즘 수업 - 피보나치 수 1
 
-def fib_Recu(n):
-    if n < 3:
-        return 1
-
-    return fib_Recu(n - 1) + fib_Recu(n - 2)
-
-def fib_DP(n):
-    dp = [1] * n
-    cnt = 0
-    
-    for i in range(2, n):
-        dp[i] = dp[i - 1] + dp[i - 2]
-        cnt += 1
-    
-    return cnt
-
 n = int(input())
-print(fib_Recu(n), fib_DP(n))
+dp = [1] * n
+
+for i in range(2, n):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[-1], n - 2)
