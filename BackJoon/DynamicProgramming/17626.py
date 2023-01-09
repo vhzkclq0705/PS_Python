14fe842a78557519d9d44acb00e72c667be6d1f0
oh_jun <vhzkclq0705@naver.com>
2023-01-09 15:18:22 +0900

    파일 이름 변경

---
+++ BackJoon/DynamicProgramming/17626.py
@@ -0,0 +1,21 @@
+# Silver 3 - Four Squares
+
+import math
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 50001
+
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, n+1):
+    min_value = 1e9
+
+    for k in range(1, math.floor(math.sqrt(i)) + 1):
+        min_value = min(min_value, dp[i - k * k])
+
+    dp[i] = min_value + 1
+
+print(dp[n])
