70d1d87027eca6e5a288e5dddcf4ebd9c07f6b44
oh_jun <vhzkclq0705@naver.com>
2023-03-27 22:42:24 -0700

    과일 장수.py

---
+++ Programmers/연습문제/LEVEL 1/과일 장수.py
@@ -0,0 +1,10 @@
+# LEVEL 1
+
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+
+    for i in range(0, len(score) - m + 1, m):
+        answer += score[i + m - 1] * m
+    
+    return answer
