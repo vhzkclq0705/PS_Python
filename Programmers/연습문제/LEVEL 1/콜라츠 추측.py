9bf03e6eac5cf3f53054469ad2732ca8ebf63d25
oh_jun <vhzkclq0705@naver.com>
2023-04-10 14:55:36 -0700

    콜라츠 추측

---
+++ Programmers/연습문제/LEVEL 1/콜라츠 추측.py
@@ -0,0 +1,12 @@
+# LEVEL 1
+
+def solution(num):
+    answer = 0
+    
+    while num != 1:
+        if answer == 500:
+            return -1
+        num = num // 2 if num % 2 == 0 else num * 3 + 1
+        answer += 1
+    
+    return answer
