c07a6f04b6954f2f5e9d7350f765a23ede22eff8
oh_jun <vhzkclq0705@naver.com>
2023-12-06 23:08:00 -0800

    1003

---
+++ BackJoon/Solved.ac 클래스/Class 3/1003.py
@@ -0,0 +1,16 @@
+# Silver 3 - 피보나치 함수
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    dp = [[0, 0] for _ in range(n + 2)]
+    dp[0] = [1, 0]
+    dp[1] = [0, 1]
+
+    for i in range(2, n + 1):
+        dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+        dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
+    
+    print(*dp[n])
