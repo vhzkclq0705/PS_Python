72879c90a4c36db12c457ea240408d1388e193d8
oh_jun <vhzkclq0705@naver.com>
2024-02-14 14:58:05 +0900

    2503

---
+++ BackJoon/after2024/BruteForce2/2503.py
@@ -0,0 +1,12 @@
+# Silver 3 - 숫자 야구
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# nums = [list(map(int, input().split())) for _ in range(n)]
+# tmp = product(list(range(1, 10)), 3)
+# ans = 0
+# print(list(tmp))
+# for num, s, b in nums:
+    
